cd848e8058df0920475742f2dc1d18d0983831f3
Alex-devalt <79976996+Alex-devalt@users.noreply.github.com>
2022-03-21 19:00:56 +0200

    lesson_3

--- todolist/settings.py
+++ todolist/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'users',
     'corsheaders',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,24 @@ AUTH_USER_MODEL = 'users.User'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+# REST_FRAMEWORK = {
+#      'DEFAULT_RENDERER_CLASSES': [
+#          'rest_framework.renderers.JSONRenderer',
+#          'rest_framework.renderers.BrowsableAPIRenderer',
+#      ]
+#  }
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    ),
+
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
+}
